ed15b639763589ac3c9ff877aadc7ceeaff852e7
alphakuka <projetrobot11@gmail.com>
2023-10-03 23:22:25 +0200

    Updated all the files

--- movie/movie.py
+++ movie/movie.py
@@ -1,6 +1,6 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
 
@@ -25,7 +25,28 @@ def playground():
     
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
-    # todo to complete
+    type_defs = load_schema_from_path('movie.graphql')
+    query = QueryType()
+    movie = ObjectType('Movie')
+    query.set_field('movie_with_id', r.movie_with_id)
+
+    mutation = MutationType()
+    mutation.set_field('update_movie_rate', r.update_movie_rate)
+
+    actor = ObjectType('Actor')
+    movie.set_field('actors', r.resolve_actors_in_movie)
+
+    schema = make_executable_schema(type_defs, movie, query, mutation, actor)
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=None,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
 
 if __name__ == "__main__":
     print("Server running in port %s"%(PORT))
